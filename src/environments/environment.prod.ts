4784ed134878dd73080d5efe82a27e5187a5eac6
Mladen <blagmash@gmail.com>
2020-10-05 11:22:31 +0200

    redy for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  api: {
+    base: "https://jsonplaceholder.typicode.com/",
+    posts: () => environment.api.base + "posts/",
+    comments: () => environment.api.base + "comments",
+    users: () => environment.api.base + "users/",
+  },
 };
